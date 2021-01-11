3a076ea3206dc9b64dd515887b1d1a8da1ad31c2
Syed Zubair <59087484+Zubair-droid@users.noreply.github.com>
2021-01-11 14:02:32 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Syed Zubair</li>
+              <li>&copy; Copyright 2021 Syed Zubair</li>
   
            </ul>
 
